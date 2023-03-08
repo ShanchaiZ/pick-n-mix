16abce0d0542fb3b6ef4aeb8df2229189165f609
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-08 18:18:16 -0500

    updated yaml file

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
