68514a8c5ec07d616c607cbd0596ac22fc21bc64
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-13 17:06:57 -0400

    host process DB_SERVER

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,12 @@ if (process.env.JAWSDB_URL) {
     process.env.MYSQL_DATABASE,
     process.env.MYSQL_USER,
     process.env.MYSQL_PASSWORD,
-    {
-      host: "mysql-1nj8",
-      dialect: "mysql",
-      port: 33060,
-    }
+    { host: process.env.DB_SERVER }
+    // {
+    //   host: "mysql-1nj8",
+    //   dialect: "mysql",
+    //   port: 3306,
+    // }
   );
 }
 
